2bfe8889ee59994bdd73203566ed5a4c4cbeba23
poojaprabhu9 <prabhuppooja@gmail.com>
2024-02-12 14:37:32 +0530

    Included snippet to recontruct the signal

--- runMP.m
+++ runMP.m
@@ -15,6 +15,8 @@ numElectrodes = length(goodElectrodes);
 folderName = fullfile(folderSourceString,'data',monkeyName,gridType,expDate,protocolName);
 folderNameMP = fullfile('../data',monkeyName,gridType,expDate,protocolName,'mpAnalysis'); % The path must be relative when using Windows (i.e., should not start with c: etc. So this part only works when we are in the programs folder)
 makeDirectory(folderNameMP);
+folderNameReconstMP = fullfile('../data',monkeyName,gridType,expDate,protocolName,'segmentedData','lfpMP');
+makeDirectory(folderNameReconstMP);
 
 tmp = load(fullfile(folderName,'segmentedData','LFP','lfpInfo.mat'),'timeVals');
 timeVals = tmp.timeVals;
@@ -25,18 +27,53 @@ Max_iterations = 500; % number of iterations
 for i=1:numElectrodes
     eNum = goodElectrodes(i);
 
-    tmp = load(fullfile(folderName,'segmentedData','LFP',['elec' num2str(eNum) '.mat']),'analogData');
-    inputSignal = tmp.analogData;
+    if ~exist(fullfile(folderNameMP,(['elec' num2str(eNum) '/GaborMP/mp0.bok.000'])), 'file')
 
-    tag = sprintf('elec%d/',eNum);
+        tmp = load(fullfile(folderName,'segmentedData','LFP',['elec' num2str(eNum) '.mat']),'analogData');
+        inputSignal = tmp.analogData;
+    
+        tag = sprintf('elec%d/',eNum);
+    
+        % Import the data
+        X(:,:,1) = inputSignal';
+        L = size(X,1);
+        signalRange = [1 L]; % full range
+        importData(X,folderNameMP,tag,signalRange,Fs);
+    
+        % perform Gabor decomposition
+        Numb_points = L; % length of the signal
+        runGabor(folderNameMP,tag,Numb_points,Max_iterations);
+        wrap=1;
 
-    % Import the data
-    X(:,:,1) = inputSignal';
-    L = size(X,1);
-    signalRange = [1 L]; % full range
-    importData(X,folderNameMP,tag,signalRange,Fs);
+        numTrials = length(gaborInfo);
 
-    % perform Gabor decomposition
-    Numb_points = L; % length of the signal
-    runGabor(folderNameMP,tag,Numb_points,Max_iterations);
+        analogData = zeros(numTrials,L);
+        
+        for itrial=1:length(gaborInfo)
+            gaborData = gaborInfo{itrial}.gaborData;
+            gaborData(:,(gaborData(2,:)==0)) = []; % Remove atoms with zero frequency
+            analogData(itrial,:) = reconstructSignalFromAtomsMPP(gaborData,L,wrap);
+        end
+
+        filetoSave=fullfile(folderNameReconstMP,['elec' num2str(eNum) '.mat']);
+        save(filetoSave,"analogData");
+        clear gaborInfo
+    else
+        tag = sprintf('elec%d/',eNum);
+        wrap = 1;
+        gaborInfo = getGaborData(folderNameMP,tag,1);
+        numTrials = length(gaborInfo);
+        L = length(timeVals);        
+        analogData = zeros(numTrials,L);
+        
+        for itrial=1:length(gaborInfo)
+            gaborData = gaborInfo{itrial}.gaborData;
+            gaborData(:,(gaborData(2,:)==0)) = []; % Remove atoms with zero frequency
+            analogData(itrial,:) = reconstructSignalFromAtomsMPP(gaborData,L,wrap);
+        end
+
+        filetoSave=fullfile(folderNameReconstMP,['elec' num2str(eNum) '.mat']);
+        save(filetoSave,"analogData");
+        clear gaborInfo
+    end
 end
